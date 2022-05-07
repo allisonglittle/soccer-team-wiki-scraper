d3a5deb1a468196268551147225304106966af4c
Alli <allisonglittle@gmail.com>
2022-05-07 08:50:15 -0700

    Initialize image scraper script

---
+++ soccer-api/image-scraper.py
@@ -0,0 +1,12 @@
+# ----------------------------------------------------------------------
+# Author: Allison Little
+# Program: Soccer Image Scraper
+# Version: 1.0.0
+# Description: Uses GET Request to obtain image source address of
+#   a specified nation's soccer team crest from Wikipedia. Returns a
+#   JSON object containing the image address.
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# Imports
+# ----------------------------------------------------------------------

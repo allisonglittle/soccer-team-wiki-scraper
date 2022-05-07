ef3a4c07dbf1f72abe9bacf301ee6a2ac33c1e28
Alli <allisonglittle@gmail.com>
2022-05-07 16:57:11 -0700

    Implemented GET request for nation's crest

--- soccer-api/image-scraper.py
+++ soccer-api/image-scraper.py
@@ -10,3 +10,40 @@
 # ----------------------------------------------------------------------
 # Imports
 # ----------------------------------------------------------------------
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+# ----------------------------------------------------------------------
+# Function takes in a country name, issues a GET request to Wikipedia
+#   and returns the image address of the country's soccer team crest.
+# ----------------------------------------------------------------------
+def crest_img_scraper(country):
+    url = 'https://en.wikipedia.org/wiki/' + country +'_national_football_team'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find first image from wikipedia page
+    image = soup.find('img', attrs={'title': 'Shirt badge/Association crest'})
+    # attrs={'class':
+    #                 'col-xs-6 col-sm-4 col-md-3 col-lg-3'}
+    img_src = []
+    images = soup.find_all('img')
+    for image in images:
+        if image['alt'] == 'Shirt badge/Association crest':
+            img_src.append(image['src'])
+            break
+    return img_src
+
+
+# ----------------------------------------------------------------------
+# Function takes in a country name and returns a JSON object containing
+#   the image address of the country's soccer team crest.
+# ----------------------------------------------------------------------
+def get_crest(country):
+    img_src = crest_img_scraper(country)
+    json_img_src = json.dumps({'crest-source': img_src[0]})
+    return json_img_src
+
+print(get_crest('Japan'))
